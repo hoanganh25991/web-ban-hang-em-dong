347a08323b7ebc78609a4e2347a55592c28ffd82
Anh Le Hoang <hoanganh_25991@yahoo.com.vn>
2016-03-15 04:52:08 +0000

    style.css, remove .ibanner

--- wp-content/themes/i-craft-1.1.4/js/calendar-highlight-today.js
+++ wp-content/themes/i-craft-1.1.4/js/calendar-highlight-today.js
@@ -2,4 +2,7 @@
 	var today_td = $("#today");
 	today_td.css("background-color", "#dd3333");
 	today_td.css("color", "#ffffff");
-} )( jQuery );
+	
+	// var close_btn_fb_chat = $("#close-chatbox-btn").find("span"); console.log(close_btn_fb_chat);
+	// close_btn_fb_chat.append($("img").attr("src", "https://cdn3.iconfinder.com/data/icons/virtual-notebook/16/button_close-128.png"));
+ } )( jQuery );
